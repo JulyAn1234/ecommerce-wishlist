debd6f9b4613e9b3da0be9d526045858ae5be822
Julian <124217830+JulyAn1234@users.noreply.github.com>
2025-01-10 19:50:09 -0800

    Update CatalogClient.java

--- src/main/java/com/ecommerceWishlist/ecommerceWishlist/feigns/CatalogClient.java
+++ src/main/java/com/ecommerceWishlist/ecommerceWishlist/feigns/CatalogClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ecommerce-catalog-public", url = "http://localhost:8083/ecommerce-catalog")
+@FeignClient(name = "ecommerce-catalog-public", url = "http://ecommerce-catalog-public:8083")
 public interface CatalogClient {
     @GetMapping("/product/{productId}")
     Product getProductFromCatalog(@PathVariable("productId") String productId);
