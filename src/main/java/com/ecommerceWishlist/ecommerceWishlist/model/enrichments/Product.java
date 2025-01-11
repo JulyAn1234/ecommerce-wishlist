1cb282cc6ad636ad3dee00896893fb72c4cd7090
Julian <124217830+JulyAn1234@users.noreply.github.com>
2025-01-10 19:58:28 -0800

    Update Product.java

--- src/main/java/com/ecommerceWishlist/ecommerceWishlist/model/enrichments/Product.java
+++ src/main/java/com/ecommerceWishlist/ecommerceWishlist/model/enrichments/Product.java
@@ -1,4 +1,9 @@
 package com.ecommerceWishlist.ecommerceWishlist.model.enrichments;
 
 public class Product {
+    private String productId;
+    private String name;
+    private Double price;
+    private String imageUrl;
+    
 }
